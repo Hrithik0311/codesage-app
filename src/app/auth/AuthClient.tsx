91927858cc90f3841206e19e69d0eb6c49d60aa5
HH “H#H_03#11” <hraha0311@gmail.com>
2025-06-24 17:12:27 +0000

    not working pull it from really really past

--- src/app/auth/AuthClient.tsx
+++ src/app/auth/AuthClient.tsx
@@ -261,7 +261,7 @@ export default function AuthClient() {
         </div>
 
         <Button onClick={handleGoogleSignIn} variant="outline" className="w-full" disabled={isLoading}>
-          <svg className="mr-2 h-4 w-4" aria-hidden="true" focusable="false" viewBox="0 0 488 512">
+          <svg className="mr-2 h-4 w-4" aria-hidden="true" viewBox="0 0 488 512">
             <path
               fill="currentColor"
               d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 126 23.4 172.9 61.9l-76.2 76.2C322.3 119.3 286.5 96 248 96c-88.8 0-160 72.2-160 160s71.2 160 160 160c98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 26.9 3.9 41.4z"
