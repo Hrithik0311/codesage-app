a6ecc477f35f5aac6318c8f7ee6e616c12ac9ac7
HH “H#H_03#11” <hraha0311@gmail.com>
2025-07-12 02:04:44 +0000

    no radius

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -59,9 +59,9 @@ export default {
         'custom-background': 'hsl(var(--custom-background))',
       },
       borderRadius: {
-        lg: 'var(--radius)',
-        md: 'calc(var(--radius) - 2px)',
-        sm: 'calc(var(--radius) - 4px)',
+        lg: '0',
+        md: '0',
+        sm: '0',
       },
       keyframes: {
         'accordion-down': {
