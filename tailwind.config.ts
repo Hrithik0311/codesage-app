522133291db7ab91ec655c8fd9ff0d964f62c2ec
HH “H#H_03#11” <hraha0311@gmail.com>
2025-07-12 02:06:47 +0000

    noo I want to add rounder corner

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -59,9 +59,9 @@ export default {
         'custom-background': 'hsl(var(--custom-background))',
       },
       borderRadius: {
-        lg: '0',
-        md: '0',
-        sm: '0',
+        lg: 'var(--radius)',
+        md: 'calc(var(--radius) - 2px)',
+        sm: 'calc(var(--radius) - 4px)',
       },
       keyframes: {
         'accordion-down': {
